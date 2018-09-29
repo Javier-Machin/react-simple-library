61b36c4a73036429ca41a21105907537581432c1
Javier Machin <javier_machin_acosta@hotmail.com>
2018-09-29 20:24:26 +0100

    Add form functionality

--- src/components/book-form.js
+++ src/components/book-form.js
@@ -1,44 +1,98 @@
 import React, { Component } from 'react';
 
 class BookForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      author: '',
+      pages: 10,
+      read: "yes"
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  handleSubmit(event) {
+    const newBook = new Book(
+      this.state.title, 
+      this.state.author, 
+      this.state.pages, 
+      this.state.read
+    );
+    
+    const books = [newBook];
+    
+    alert(
+      `
+       Title: ${books[0].title} 
+       Author: ${books[0].author} 
+       Pages: ${books[0].pages} 
+       Read: ${books[0].read}
+      ` 
+    );
+    
+    event.preventDefault();
+  }
 
   render() {
     return (
-      <form className="book-form">
+      <form className="book-form" onSubmit={this.handleSubmit}>
         <label>Title:</label><br />
-        <input type="text" name="title" id="form-title" placeholder="The Book" maxLength="15" /><br />
+        <input type="text" 
+               name="title" 
+               id="form-title" 
+               value={this.state.name} 
+               onChange={this.handleChange} 
+               maxLength="15" /><br />
+        
         <label>Author:</label><br />
-        <input type="text" name="author" id="form-author" placeholder="John Smith" maxLength="15" /><br />
+        <input type="text" 
+               name="author" 
+               id="form-author" 
+               value={this.state.author} 
+               onChange={this.handleChange} 
+               maxLength="15" /><br />
+        
         <label>Pages:</label><br />
-        <input type="number" name="pages" id="form-pages" placeholder="100" min="1" max="9999" /><br />
+        <input type="number" 
+               name="pages" 
+               id="form-pages" 
+               value={this.state.pages} 
+               onChange={this.handleChange} 
+               min="10" 
+               max="9999" /><br />
+        
         <label>Read:</label><br />
-        <input type="radio" id="yes" value="true" name="read" />
-        <label htmlFor="yes">Yes</label><br />
-        <input type="radio" id="no" value="false" name="read" />
-        <label htmlFor="no">No</label><br /><br />
-        <BtnSubmitForm />
+        
+        <input type="radio" 
+               id="readYes" 
+               value="yes" 
+               checked={this.state.read === 'yes'} 
+               onChange={this.handleChange} 
+               name="read" />
+        <label htmlFor="readYes">Yes</label><br />
+        
+        <input type="radio" 
+               id="readNo" 
+               value="no" 
+               checked={this.state.read === 'no'} 
+               onChange={this.handleChange} 
+               name="read" />
+        <label htmlFor="readNo">No</label><br /><br />
+        
+        <input type="submit" value="Submit" />
       </form>
     );
   }
 }
 
-class BtnSubmitForm extends Component {
-  handleClick() {
-    const theGame = new Book("The Game", "Straus", 580, "false");
-    books.push(theGame);
-    console.log(books[2]);
-  }
-
-  render() {
-    // This syntax ensures `this` is bound within handleClick
-    return (
-      <button className="btn btn-form-submit" type="button" onClick={(e) => this.handleClick(e)}>
-        Submit
-      </button>
-    );
-  }
-}
-
+// Transform to component ?
 class Book {
   constructor(title, author, pages, read) {
     this.title = title;
@@ -49,10 +103,6 @@ class Book {
 
   toggleRead() { this.read === "true" ? this.read = "false" : this.read = "true" }
 }
-
-const quixote = new Book("Don Quixote", "Cervantes", 980, "false");
-const artOfWar = new Book("The Art of War", "Sun Tzu", 130, "false");
-const books = [quixote, artOfWar];
 
 
 export default BookForm
